3cd2e9351368f2a700e7c37d07db8970101ccb5e
gnaneshwar86 <gnaneshwarnani8605@gmail.com>
2026-02-12 22:37:22 +0530

    initial setup initiated with dataset

---
+++ src/extract_frames.py
@@ -0,0 +1,36 @@
+import cv2
+import os
+
+VIDEO_DIR = "dataset/raw_videos"
+FRAME_DIR = "dataset/frames"
+FRAME_RATE = 5   # take 1 frame every 5 frames
+
+os.makedirs(FRAME_DIR, exist_ok=True)
+
+for label in os.listdir(VIDEO_DIR):
+    label_path = os.path.join(VIDEO_DIR, label)
+    frame_label_path = os.path.join(FRAME_DIR, label)
+    os.makedirs(frame_label_path, exist_ok=True)
+
+    for video in os.listdir(label_path):
+        cap = cv2.VideoCapture(os.path.join(label_path, video))
+        count = 0
+        saved = 0
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            if count % FRAME_RATE == 0:
+                frame = cv2.resize(frame, (224, 224))
+                cv2.imwrite(
+                    f"{frame_label_path}/{video}_{saved}.jpg",
+                    frame
+                )
+                saved += 1
+            count += 1
+
+        cap.release()
+
+print("✅ Frame extraction completed")
